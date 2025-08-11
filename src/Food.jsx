5715e03be6ad457f553e161e9ed814490ea0cb83
Perry Huang <perryhuang2015@gmail.com>
2025-08-11 09:26:03 -0400

    feat: Add prettierrc and food

---
+++ src/Food.jsx
@@ -0,0 +1,9 @@
+export function Hate() {
+    return <h1>I eat to live, not live to eat: therefore I hate food</h1>
+}
+export function Love() {
+    return <h1>Pizza is great</h1>
+}
+export function Food() {
+    return <h1>Food!</h1>
+}
