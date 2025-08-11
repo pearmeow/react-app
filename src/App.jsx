bd870e732633239591ddcca364d47e77c4b8aacb
Perry Huang <perryhuang2015@gmail.com>
2025-08-11 17:41:33 -0400

    feat: practice react stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import './App.css'
-
-function ListItem(props) {
-  return <li>{props.animal}</li>;
-}
+import "./App.css";
 
 function List(props) {
-  return (
-    <ul>
-      {props.animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />
-      })}
-    </ul>
-  )
+    if (!props.animals) {
+        return <div>Loading...</div>;
+    }
+    if (props.animals.length === 0) {
+        return <div>There are no animals in the list!</div>;
+    }
+    return (
+        <ul>
+            {props.animals.map((animal) => {
+                return <li key={animal}>{animal}</li>;
+            })}
+        </ul>
+    );
 }
 
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
-  return (
-    <>
-      <h1>Animals: </h1>
-      <List animals={animals} />
-    </>
-  )
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+    return (
+        <>
+            <h1>Animals: </h1>
+            <List animals={animals} />
+        </>
+    );
 }
 
-export default App
+export default App;
