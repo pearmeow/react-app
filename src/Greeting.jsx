8b1b1fe26dc07e5a402fcd5987ba0aeda256ab65
Perry Huang <perryhuang2015@gmail.com>
2025-08-11 09:26:17 -0400

    feat: update greeting and main

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,3 +1,3 @@
-function Greeting() {
+export default function Greeting() {
     return <h1>&quot;I swear by my pretty floral bonnec, I will end you.&quot;</h1>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Greeting from './Greeting.jsx';
+import { Food, Hate, Love } from './Food.jsx';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Hate />
+    <Food />
+    <Love />
+    <Greeting />
   </StrictMode>,
 )
